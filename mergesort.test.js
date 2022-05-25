57b3871db57cab3547fdbd07ddd70d3589779891
couvq <quentincouvelaire@gmail.com>
2022-05-25 15:05:27 -0400

    added another test case with duplicates just to make sure

--- mergesort.test.js
+++ mergesort.test.js
@@ -5,3 +5,8 @@ test('array [3, 2, 1] will be sorted to [1, 2, 3]', () => {
     let result = mergesort([3, 2, 1]);
     expect(result).toEqual([1, 2, 3]);
   });
+
+  test('array [5, 7, 9, 8, 4, 4, 10] will be sorted to [4, 4, 5, 7, 8, 9, 10]', () => {
+    let result = mergesort([5, 7, 9, 8, 4, 4, 10]);
+    expect(result).toEqual([4, 4, 5, 7, 8, 9, 10]);
+  });
